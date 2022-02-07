6118a393ff48e09c36dd9a87807fc5a4718bcd2f
Rodolfo Gonzalez <rgonzalezled@gmail.com>
2022-02-06 21:23:46 -0300

    se agrego boton pagar en carrito, se habilita cuando hay productos en el carrito

--- js/main.js
+++ js/main.js
@@ -22,14 +22,14 @@ class Producto {
 const productos = [
     new Producto(1, 'NBA2K22', 5000, 'deportes', 2022, 'Español', './images/nba-2k22.jpg', 2),
     new Producto(2, 'FIFA22', 8000, 'deportes', 2022, 'Español', './images/fifa22.jpg', 5),
-    new Producto(3, 'Crash Ctr', 7000, 'conduccion', 2020, 'Español', './images/crash-ctr.jpg', 4),
+    new Producto(3, 'Resident Evil 3', 5500, 'accion', 2021, 'Inglés', './images/resident-evil-3.jpg', 5),
     new Producto(4, 'Sims 4', 2000, 'simulacion', 2019, 'Español', './images/sims4.jpg', 3),
     new Producto(5, 'Need for Speed', 3000, 'conduccion', 2022, 'Español', './images/need-for-speed.jpg', 8),
-    new Producto(6, 'Mortal Kombat 11', 2500, 'combate', 2021, 'Inglés', './images/mortal-kombat.jpg', 5),
+    new Producto(6, 'Crash Ctr', 7000, 'conduccion', 2020, 'Español', './images/crash-ctr.jpg', 4),
     new Producto(7, 'Uncharted', 2500, 'accion', 2019, 'Español', './images/uncharted4.jpg', 5),
     new Producto(8, 'Horizon II', 10000, 'accion', 2022, 'Inglés', './images/horizon.jpg', 2),
     new Producto(9, 'Far Cry 6', 9000, 'accion', 2022, 'Español', './images/farcry6.jpg', 1),
-    new Producto(10, 'Resident Evil 3', 5500, 'accion', 2021, 'Inglés', './images/resident-evil-3.jpg', 5),
+    new Producto(10, 'Mortal Kombat 11', 2500, 'combate', 2021, 'Inglés', './images/mortal-kombat.jpg', 5),
     new Producto(11, 'Spiderman', 4500, 'accion', 2020, 'Español', './images/spider-man.jpg', 5),
     new Producto(12, 'It Takes Two', 7000, 'simulacion', 2021, 'Inglés', './images/it-takes-two.jpg', 2),
 ];
@@ -37,7 +37,9 @@ const productos = [
 const carrito = [];
 let total = 0;
 crearCardProductos(productos);
-
+const btnPagar = document.getElementById('pagar');
+btnPagar.disabled = true; 
+btnPagar.addEventListener("click", calcularTotal);
 //-------------- Agregar al Carrito -------------------------------------------------------------------
 
 function agregarCarrito() {
@@ -50,7 +52,8 @@ function agregarCarrito() {
         mostrarCarrito.innerHTML = "";
         for (const producto of carrito) {
             let li = document.createElement("li");
-            li.textContent = producto.nombre;    
+            li.textContent = `${producto.nombre} - Precio: ${producto.precio}`;
+            btnPagar.disabled = false;    
             mostrarCarrito.appendChild(li);   //--------- Mostrar carrito ----------------------------
         }
 
@@ -73,10 +76,12 @@ function calcularTotal() {
         if (carrito.length > 5) {
             console.log(`El monto total a pagar es: ${total}`); //Precio sin descuento
             console.log('¡FELICIDADES! Obtuviste un descuento del 20%'); //Precio con descuento eligiendo 6 unidades o mas
+            alert('¡FELICIDADES! Obtuviste un descuento del 20%'); //Precio con descuento eligiendo 6 unidades o mas
             total = total - (total * 0.20);
         } else if (carrito.length > 2) {
             console.log(`El monto total a pagar es: ${total}`); //Precio sin descuento
             console.log('¡FELICIDADES! Obtuviste un descuento del 10%'); //Precio con descuento eligiendo de 3 a 5 unidades 
+            alert('¡FELICIDADES! Obtuviste un descuento del 10%'); //Precio con descuento eligiendo de 3 a 5 unidades 
             total = total - (total * 0.10);
         }
 
